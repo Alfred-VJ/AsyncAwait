fb9e81777df14c9de9e4a86e6bcaf7f175084894
ALFRED-VJ-DEV <alfred.vj.dev@gmail.com>
2024-10-13 10:20:14 -0600

    firsth example

--- Ejercicios/Ejercicio01.js
+++ Ejercicios/Ejercicio01.js
@@ -1,3 +1,7 @@
-async function miFn(){
-    
+async function getAllContries(){
+    let response = await fetch("https://restcountries.com/v3.1/all");
+    let data = await response.json();
+    console.log({data});
 }
+
+getAllContries();
