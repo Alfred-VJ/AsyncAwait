32dd1a9f72b4d830e91a861c3c1f2eceaace230d
EAMendoza90 <regis.lucis.1990@gmail.com>
2024-10-13 11:05:57 -0600

    incompleto

--- Ejercicios/Ejercicio01.js
+++ Ejercicios/Ejercicio01.js
@@ -4,4 +4,39 @@ async function getAllContries(){
     console.log({data});
 }
 
+function renderCountries(countries) {
+    const container = document.getElementById("countriesContainer");
+
+    
+    for (let i = 0; i < countries.length; i++) {
+        const country = countries[i];
+
+        
+        const card = document.createElement("div");
+        card.classList.add("country-card");
+
+        const flag = document.createElement("img");
+        flag.src = country.flags?.png || "ruta/imagen/por/defecto.png"; 
+        flag.alt = `Bandera de ${country.name.common}`;
+        flag.classList.add("country-flag");
+
+        const name = document.createElement("h2");
+        name.textContent = country.name.common;
+        name.classList.add("country-name");
+
+        const description = document.createElement("p");
+        description.textContent = `Capital: ${country.capital?.[i] || "N/A"}`;
+        description.classList.add("country-description");
+
+        
+        card.appendChild(flag);
+        card.appendChild(name);
+        card.appendChild(description);
+
+    console.log("******************")        
+        container.appendChild(card);
+    }
+}
+
+
 getAllContries();
